2d404f57803eea0aea0cbc6d290c5b2848f4d15b
MUKILTEO\1721537 <1721537@mukilteo.wednet.edu>
2025-02-21 09:21:12 -0800

    Movement and jumping

--- Project/Assets/Scripts/PlayerMovement.cs
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -1,33 +1,45 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float velocity;
+    private Rigidbody2D _rb;
+    [SerializeField] private float jumpForce;
+    private Vector2 jump;
+    private bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+
+        jump = new Vector2(_rb.linearVelocityX, _rb.linearVelocityY = jumpForce);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            velocity = 10;
+        }
+        else
+        {
+            velocity = 5;
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector2(-1,0);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            Vector2 up = new Vector2(0, 1) * velocity;
+            _rb.linearVelocityX = -velocity;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Vector2 right = new Vector2(1, 0) * velocity;
+            _rb.linearVelocityX = velocity;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
-            Vector2 down = new Vector2(0, -1) * velocity;
+            _rb.AddForce(jump * 4);
         }
     }
 }
