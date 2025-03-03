9e4737d21731c5d969e3bf20ef41e53c1df3dca3
MUKILTEO\1721537 <1721537@mukilteo.wednet.edu>
2025-03-03 10:16:19 -0800

    Unity Events

--- Project/Assets/Scripts/ClickBehavior.cs
+++ Project/Assets/Scripts/ClickBehavior.cs
@@ -5,12 +5,22 @@ using UnityEngine.SceneManagement;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public UnityEvent onClick;
+    public UnityEvent onCollision;
+    public GameObject preFab;
+    public Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            CreateFriend();
+
             var raycast = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(raycast))
             {
@@ -19,8 +29,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        onCollision.Invoke();
+    }
+
     public void SwitchScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void CreateFriend()
+    {
+        Instantiate(preFab, cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z + 1)), Quaternion.identity);
     }
 }
---
+++ Project/Assets/Scripts/WaveController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class WaveController : MonoBehaviour
+{
+    public List<WaveData> waveData;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        /*
+        for (int i = 0; 1 < waveData.enemyCount; ++i)
+        {
+            Instantiate(waveData.enemyPreFab);
+        }
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
