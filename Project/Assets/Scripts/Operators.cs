caaa19df836291331502a9d740a028e422bf6d31
MUKILTEO\1721537 <1721537@mukilteo.wednet.edu>
2025-04-21 07:49:56 -0700

    Please don't break

--- Project/Assets/Scripts/Operators.cs
+++ Project/Assets/Scripts/Operators.cs
@@ -3,28 +3,23 @@ using UnityEngine;
 public struct Operations
 {
     public Vector2 input { get; set; }
-
     public static Operations operator +(Operations value1, Operations value2)
     {
         return new Operations { input = value1.input + value2.input };
     }
-
     public static Operations operator -(Operations value1, Operations value2)
     {
         return new Operations { input = value1.input - value2.input };
     }
-
     public static Operations operator *(Operations value1, Operations value2)
     {
         return new Operations { input = value1.input * value2.input };
     }
-
     public static Operations operator /(Operations value1, Operations value2)
     {
         return new Operations { input = value1.input / value2.input };
     }
 }
-
 public class Operators : MonoBehaviour
 {
     static Operations solve1 = new Operations { input = new Vector2(5, 0) };
@@ -32,10 +27,7 @@ public class Operators : MonoBehaviour
     static Operations solve3 = new Operations { input = new Vector2(-1, -6) };
 
     Operations sum = solve1 + solve3;
-
     Operations minus = solve2 - solve3;
-
     Operations multi = solve1 * solve2;
-
     Operations divide = solve3 / solve2;
 }
